e225b1ba0778bc0eeada0cbb24a27f463bab0e7a
tajogi <120653132+tajogitajogi@users.noreply.github.com>
2023-01-12 21:37:29 +0500

    Add files via upload

---
+++ task/taskmanager/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Task(models.Model):
+    title = "Task"
+    task= models.CharField('task',max_length=30)
+    date= models.CharField('Начальная дата',max_length=10)
+    time= models.CharField('Время',max_length=2)
+    stop=0
+    
+    def __str__(self):
+        return self.title
+
+
